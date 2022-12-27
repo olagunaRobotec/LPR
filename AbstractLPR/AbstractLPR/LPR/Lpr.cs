2586091b51fe720782ea914ee094025507d0cf1d
Oscar Laguna <oleo920608@gmail.com>
2022-12-27 09:57:30 -0500

    LPRabstractEjemplo

---
+++ AbstractLPR/AbstractLPR/LPR/Lpr.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractLPR.LPR
+{
+    abstract class Lpr
+    {
+        public abstract void GetToken(string url, string token);
+        public abstract void Authentication();
+        public abstract void RecognizerPlate();
+    }
+    class Token : Lpr
+    {
+        public override void GetToken(string url, string token)
+        {
+            Console.WriteLine("hola Dayron su url es " + url + ",y su token es " + token);
+        }
+        public override void Authentication()
+        {
+            Console.WriteLine("hola Dayron Authentication");
+        }
+        public override void RecognizerPlate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class AuthenticationE : Lpr
+    {
+        public override void GetToken(string url, string token)
+        {
+            Console.WriteLine("Adios Dayron su url es " + url + ",y su token es " + token);
+        }
+
+        public override void Authentication()
+        {
+            Console.WriteLine("Adios Dayron Authentication");
+        }
+
+        public override void RecognizerPlate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AbstractLPR/AbstractLPR/Program.cs
@@ -0,0 +1,30 @@
+﻿using AbstractLPR.LPR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractLPR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://192.68.1.129";
+            string token = "tokenvfvdgdbfhbdthr";
+
+            Token T1 = new Token();
+            T1.GetToken(url,token);
+            T1.Authentication();
+
+            AuthenticationE A1 = new AuthenticationE();
+            string _url = "http://fjeshgdruhgiudh";
+            string _token = "tokengkdogjdoxjgodbxdbtrdx";
+            A1.GetToken(_url, _token);
+            A1.Authentication();
+
+            Console.ReadLine();
+        }
+    }
+}
